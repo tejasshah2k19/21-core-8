077635f2436328badf5c15219e91cff53c3c9080
Tejas <tejas14all@gmail.com>
2021-07-26 21:01:07 +0530

    first session

---
+++ Calc.java
@@ -0,0 +1,40 @@
+class Calc{
+
+
+	public static void main(String arg[]){
+	
+		int a =90; //a is variable and int is datatype 
+		int b= 100; 
+		int ans = a+b;
+		//printf 
+		//cout 
+		System.out.println(ans);
+		//scanf 
+		//cin 
+		//lib --> classes{Scanner}{InputStreamReader}{}{}{}{}{}{}
+
+	}
+}
+
+//variable --> variable is dataname , which can holds value. -> it can be changed. 
+		// it ocupies some storage in memory 
+
+//datatype --> what kind of data can be hold by a variable 
+//how many types of datatype supported by java? 
+//2 types 
+//primitive - 8 
+//non-primitive - n 
+
+/*
+	primitive 
+	
+	byte(non fraction digit)	1 byte	8bit 2^8 => 256 => -128 0 127 
+	short(nfd) 			2 byte  16bit 2^16 => 65536 => -32768 32767
+	char (single character) 	2 byte 
+	int(nfd)   			4 byte 32bits 2^32 =>   
+	long(nfd)   			8 byte 
+	float(real)    			4 byte 
+	double(real)    		8 byte 
+	boolean (true|false) 		1 bit 
+	
+*/ 
---
+++ Std.java
@@ -0,0 +1,86 @@
+/*
+//datatypes 
+
+//class - blueprint -- logical -- group | logical |    
+
+//object - physical , entity - real world instance -- member | physical 
+
+
+Dog  ->   
+
+Fruit --> SummerFruit ---> Mango -->  KesarMango  --> GKM  --> YKM  
+
+
+DhirajSir --> object 
+Royal-->class 
+
+Projector --> class 
+
+Human -->Men ---> dhiraj sir 
+
+Bank --> class -> Account --> SavingAccount  CurrentAccount 
+//				object 
+
+//class --> properties --> state & behaviour 
+		       //->  variable & methods (function)  
+			//-> int a; 
+		 	//-> void add() 
+
+//class -> properties --> object -> use 
+*/
+class Std{
+	public static void main(String sd[]){
+
+		System.out.println("hello"); 
+	}
+
+}
+class A{
+
+}
+//javac - java file compile  
+//java  - execute class file 
+
+
+//class  -> Std , String , System 
+//method -> main() println()
+//variable/objects -> out , args , 
+
+//naming convention :- 
+	//class ->   first character capital , Employee , Student , TempEmpl
+	//method - variable - object ---> first char small ,  add() ,  salary ,  addEmpl() , totalSal;  
+
+	//const ->   once we assign value we can not modify --
+	//final --> all caps ==>  PI , MAX , MIN_BAL ,
+
+//when we compile .c file what we get???
+//no error 
+// .exe .obj .bak  ?? 1 
+
+//when we compile .java file what we get??? 
+//.java --> .class --> ?? n number --> depends on number of class you have in java file 
+// we can have n number class in our java file 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
